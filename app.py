818e0578f6ea52a38114abd0b14af480def996cc
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-06-02 11:06:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_file('index.html')
 
-@app.route('/get-data')
+@app.route('/get_data')
 def get_data():
-    return render_template('get_data.html')
+    return send_file('get_data.html')
 
 @app.route('/delete')
 def delete():
-    return render_template('delete.html')
+    return send_file('delete.html')
 
 @app.route('/submitteddata')
-def submitteddata():
-    return render_template('submitteddata.html')
+def submitted():
+    return send_file('submitteddata.html')
 
 @app.route('/data')
 def data():
-    return render_template('data.html')
+    return send_file('data.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
